5f9513a2ed32ed95fbce991c8f9b0eac2f1e3fe6
ivanChacon <ivanchacon839@gmail.com>
2022-08-10 20:30:40 -0500

    feature2

--- app/src/main/java/com/example/actividadd19/utilidades/utilidades.java
+++ app/src/main/java/com/example/actividadd19/utilidades/utilidades.java
@@ -9,12 +9,14 @@ public class utilidades {
     public static final String CAMPO_AUTOR="autor";
     public static final String CAMPO_EDITORIAL="editorial";
     public static final String CAMPO_ANIO="anio";
+    public static final String CAMPO_PRECIO="precio";
+    public static final String CAMPO_CATEGORIA="categoria";
     public static final String CAMPO_IMG="img";
 
 
     public static final String CREAR_TABLA_LIBROS="CREATE TABLE " +
             ""+TABLA_LIBROS+" ("+CAMPO_ID +" INTEGER PRIMARY KEY AUTOINCREMENT,"+CAMPO_TITULO+" TEXT, "+CAMPO_AUTOR+" TEXT, "+CAMPO_EDITORIAL+" TEXT, " +
-            ""+CAMPO_ANIO+" TEXT, "+CAMPO_IMG+" BLOP)";
+            ""+CAMPO_ANIO+" TEXT, "+CAMPO_IMG+" BLOP,"+CAMPO_PRECIO+" TEXT,"+CAMPO_CATEGORIA+"TEXT)";
 
 
 }
